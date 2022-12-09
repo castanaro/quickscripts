edda01810c3f46dbb689349d030db449dbc83675
Michael Castanaro <66240320+castanaro@users.noreply.github.com>
2022-12-09 01:50:51 -0500

    Create ipRangeChecker.py

---
+++ ipRangeChecker.py
@@ -0,0 +1,45 @@
+import sys
+
+from netaddr import IPNetwork, IPAddress
+
+print("ipcheck.py takes a supplied file of IP addresses and either a CIDR range (e.g., 192.168.10.0/24) or a file containing CIDR ranges")
+
+if len(sys.argv) != 3:
+    print("Please provide a file of IP  addresses and a CIDR range (or file containing CIDR ranges).")
+    print("Usage: python3 ipcheck.py range.txt 192.168.10.0/24 (or cidr.txt)")
+
+ipfile = sys.argv[1]
+netrange = sys.argv[2]
+
+if "/" in sys.argv[2]:
+    f = open(ipfile, "r+")
+    for i in f:
+        if i in IPNetwork(netrange):
+            print(i)
+        else:
+            continue
+    f.close()
+
+else:
+    hold = []
+    f = open(ipfile, "r+")
+    for i in f:
+        hold.append(i)
+
+    f.close()
+
+    hold2 = []
+    t = open(netrange, "r+")
+    for i in t:
+        hold2.append(i)
+
+    t.close()
+
+    print(hold2)
+
+    for i in hold:
+        for p in hold2:
+            if i in IPNetwork(str(p)):
+                print(i)
+            else:
+                continue
